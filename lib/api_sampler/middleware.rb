96e11d7e9b1637cc248bdb98f90da3f3b4964d6b
smaximov <s.b.maximov@gmail.com>
2016-10-23 23:59:09 +1000

    Implement unconditional collecting of samples

--- lib/api_sampler/middleware.rb
+++ lib/api_sampler/middleware.rb
@@ -9,7 +9,24 @@ module ApiSampler
     end
 
     def call(env)
-      @app.call(env)
+      request = Rack::Request.new(env)
+      status, headers, response = @app.call(env)
+      collect_sample(request, response)
+      [status, headers, response]
+    end
+
+    # Collects a sample of the current request.
+    #
+    # @param request [Rack::Request] current request.
+    # @param response [ActiveDispatch::Response::RackBody] response body.
+    def collect_sample(request, response)
+      endpoint = ApiSampler::Endpoint.find_or_create_by!(path: request.path)
+      endpoint.samples.create!(request_method: request.request_method,
+                               query: request.query_string,
+                               request_body: request.body.read,
+                               response_body: response.body)
+    rescue ActiveRecord::RecordInvalid => error
+      Rails.logger.error "api_sampler :: collect_sample :: #{error}"
     end
   end
 end
