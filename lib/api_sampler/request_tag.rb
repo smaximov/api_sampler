92e6409440ee8fa8635b2106349a31e525244230
smaximov <s.b.maximov@gmail.com>
2016-10-25 21:31:07 +1000

    Rename ApiSampler::RequestTag#tag to ApiSampler::RequestTag#name

--- lib/api_sampler/request_tag.rb
+++ lib/api_sampler/request_tag.rb
@@ -13,12 +13,12 @@ module ApiSampler
   #
   #   @raise [ArgumentError] if arguments are invalid.
   #
-  # @!attribute [rw] tag
-  #   @return [#to_s] a non-blank tag
+  # @!attribute [rw] name
+  #   @return [#to_s] tag name
   #
   # @!attribute [rw] matcher
   #   @return [RequestMatcher] the corresponding request matcher
-  RequestTag = Struct.new('RequestTag', :tag, :matcher) do
+  RequestTag = Struct.new('RequestTag', :name, :matcher) do
     def initialize(tag, matcher)
       raise ArgumentError, 'tag cannot be blank' if tag.blank?
       raise ArgumentError, 'matcher must be a ApiSampler::RequestMatcher' unless
